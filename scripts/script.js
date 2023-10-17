63a1bb4e7a188fdfaabaf677505ec0824913215c
Haaris <haaris@trainingbasket.in>
2023-10-17 18:47:32 +0530

    On-click section change functionality done

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,32 @@
 let controls = document.querySelectorAll(".control")
+let sections = document.querySelectorAll(".section")
 controls.forEach((element) => {
     element.addEventListener("click", (e) => {
-        e.target.style.transition = "all .3s"
+        e.target.style.transition = "all .5s"
         let currentActive = document.querySelector(".controls li.active")
         currentActive.classList.remove("active")
         e.target.classList.add("active")
+
+        for(let i = 0; i < controls.length; i++) {
+            if (controls[i].classList.contains("active")) {
+                sections[i].classList.add("active")
+                console.log("Activated");
+            }
+            else {
+                sections[i].classList.remove("active")
+                console.log("Deactivated");
+            }
+        }
     })
 })
+
+// for(let i = 0; i < controls.length; i++) {
+//     if (controls[i].classList.contains("active")) {
+//         sections[i].classList.add("active")
+//         console.log("Activated");
+//     }
+//     else {
+//         sections[i].classList.remove("active")
+//         console.log("Deactivated");
+//     }
+// }

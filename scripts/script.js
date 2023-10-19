2e5f54039f78138399c7b4a4240ea28868cd015d
Haaris <haaris@trainingbasket.in>
2023-10-19 16:46:10 +0530

    General Tweaking

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,34 @@
-let controls = document.querySelectorAll(".control")
-let sections = document.querySelectorAll(".section")
-controls.forEach((element) => {
-    element.addEventListener("click", (e) => {
-        e.target.style.transition = "all .5s"
-        let currentActive = document.querySelector(".controls li.active")
-        currentActive.classList.remove("active")
-        e.target.classList.add("active")
+// let controls = document.querySelectorAll(".control")
+// let sections = document.querySelectorAll(".section")
+// controls.forEach((element) => {
+//     element.addEventListener("click", (e) => {
+//         e.target.style.transition = "all .5s"
+//         let currentActive = document.querySelector(".controls li.active")
+//         currentActive.classList.remove("active")
+//         e.target.classList.add("active")
 
-        for(let i = 0; i < controls.length; i++) {
-            if (controls[i].classList.contains("active")) {
-                sections[i].classList.add("active")
-                console.log("Activated");
-            }
-            else {
-                sections[i].classList.remove("active")
-                console.log("Deactivated");
-            }
-        }
-    })
-})
+//         for(let i = 0; i < controls.length; i++) {
+//             if (controls[i].classList.contains("active")) {
+//                 sections[i].classList.add("active")
+//                 console.log("Activated");
+//             }
+//             else {
+//                 sections[i].classList.remove("active")
+//                 console.log("Deactivated");
+//             }
+//         }
+//     })
+// })           // Alternative 1
 
-// for(let i = 0; i < controls.length; i++) {
-//     if (controls[i].classList.contains("active")) {
-//         sections[i].classList.add("active")
-//         console.log("Activated");
-//     }
-//     else {
-//         sections[i].classList.remove("active")
-//         console.log("Deactivated");
-//     }
-// }
+const controlButtons = document.querySelectorAll(".control");
+const sections = document.querySelectorAll(".section");
+
+controlButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        controlButtons.forEach((btn) => btn.classList.remove("active"));
+        sections.forEach((sec) => sec.classList.remove("active"));
+
+        button.classList.add("active");
+        sections[index].classList.add("active");
+    });
+});             // Alternative 2
